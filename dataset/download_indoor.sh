1e14f3a22ede2cec5cb0c5e6f850448f72619d49
Dizy <dev@dizy.cc>
2022-11-07 02:13:57 -0500

    Dataset: update downloading script

--- dataset/download_indoor.sh
+++ dataset/download_indoor.sh
@@ -8,22 +8,22 @@ mkdir -p "${download_dir}"
 rm -rf "${raw_dir}"
 
 # Download the dataset
-print "Downloading the dataset..."
+echo "Downloading the dataset..."
 wget "http://groups.csail.mit.edu/vision/LabelMe/NewImages/indoorCVPR_09.tar" -P "${download_dir}"
 
 mkdir -p "${download_dir}/indoorCVPR_09"
 
-print "Extracting the dataset..."
+echo "Extracting the dataset..."
 # Extract the dataset
 tar -xvf "${download_dir}/indoorCVPR_09.tar" -C "${download_dir}/indoorCVPR_09"
 
 # Move the dataset to the raw directory
-print "Moving the dataset to the raw directory..."
+echo "Moving the dataset to the raw directory..."
 mv "${download_dir}/indoorCVPR_09/Images/" "$raw_dir"
 
 # Remove the downloaded file and the extracted folder
-print "Removing the downloaded file and the extracted folder..."
+echo "Removing the downloaded file and the extracted folder..."
 rm "${download_dir}/indoorCVPR_09.tar"
 rm -rf "${download_dir}/indoorCVPR_09"
 
-print "- Done."
+echo "- Done."
