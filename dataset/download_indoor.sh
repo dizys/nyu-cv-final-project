af9a29e1e5e2be0816ac447b63a1f6db02577f91
Dizy <dev@dizy.cc>
2022-11-07 01:58:48 -0500

    Dataset: update downloading script

--- dataset/download_indoor.sh
+++ dataset/download_indoor.sh
@@ -1,14 +1,29 @@
 #!/usr/bin/env bash
 
 shell_dir=$(dirname "$0")
+raw_dir=${shell_dir}/raw
+download_dir=${shell_dir}/download
 
-mkdir -p "$shell_dir/raw"
+mkdir -p "${download_dir}"
+rm -rf "${raw_dir}"
 
 # Download the dataset
-wget "http://groups.csail.mit.edu/vision/LabelMe/NewImages/indoorCVPR_09.tar" -P "$shell_dir"
+print "Downloading the dataset..."
+wget "http://groups.csail.mit.edu/vision/LabelMe/NewImages/indoorCVPR_09.tar" -P "${download_dir}"
 
+mkdir -p "${download_dir}/indoorCVPR_09"
+
+print "Extracting the dataset..."
 # Extract the dataset
-tar -xvf indoorCVPR_09.tar -C "$shell_dir/raw"
+tar -xvf "${download_dir}/indoorCVPR_09.tar" -C "${download_dir}/indoorCVPR_09"
 
-# Remove the compressed file
-rm "$shell_dir/indoorCVPR_09.tar"
+# Move the dataset to the raw directory
+print "Moving the dataset to the raw directory..."
+mv "${download_dir}/indoorCVPR_09/Images/" "$raw_dir"
+
+# Remove the downloaded file and the extracted folder
+print "Removing the downloaded file and the extracted folder..."
+rm "${download_dir}/indoorCVPR_09.tar"
+rm -rf "${download_dir}/indoorCVPR_09"
+
+print "- Done."
